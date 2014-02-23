3e17782d0a25116e144a3489e40f6131930dfe37
nitehack <nitehack93@gmail.com>
2014-02-23 11:40:12 +0100

    change

--- Software/PIC-16f1933/main.c
+++ Software/PIC-16f1933/main.c
@@ -9,12 +9,24 @@
 #use rs232(baud=8929,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8, ERRORS)
 
 //Defines
-#define trigger1 PIN_C0//PIN_A1
+#define trigger1 PIN_C0
 #define trigger2 PIN_A7
+
+//---------------------------
+//Receptor IR
+//ID_MANDO                   /*0XFB04*/   |   /*0XBF00*/
+#define  IR_DRCH_A  0x3067    /*0xA45B*/   |   /*0xE51A*/
+#define  IR_DRCH_M  0xD097    /*0xF906*/   |   /*0xE916*/
+#define  IR_DRCH_C  0x9037    /*0xBA45*/   |   /*0xED12*/
+#define  IR_IZDA_A  0xD017    /*0xD728*/   |   /*0xE718*/
+#define  IR_IZDA_M  0x50D7    /*0xF807*/   |   /*0xEB14*/
+#define  IR_IZDA_C  0x1077    /*0xBC43*/   |   /*0xEF10*/
+#define  IR_INIC    0x90B7    /*0x4FB0*/   |   /*0xEE11*/
+#define  IR_PARAR   0x5057    /*0x45BA*/   |   /*0xEA15*/
+//---------------------------
 
 int primero=1;
 int16 dato=0;
-int8 dir=0;
 int flanco_sub1=1;
 int flanco_sub2=1;
 int16 Tbajada;
@@ -58,21 +70,6 @@ void RDA(){
       primero=1;
    }
 
-//!   if(primero){
-//!      dato=getc();
-//!      //dato=dato<<8;
-//!      primero=0;
-//!      write_eeprom(dir,dato);
-//!      dir++;
-//!   }
-//!   else{
-//!      dato=getc();
-//!      write_eeprom(dir,dato);
-//!      dir++;
-//!      //dato=dato+getc();
-//!      primero=1;
-//!   }
-   
 
 }
 
@@ -110,10 +107,10 @@ void main()
 {
 /*Variables*/
    int dir=0;
+   int8 orden=0;
    int16 tiempo;
    int16 distancia;
    int8 i=0;
-   float y; //relacion
    int16 distancia1=1, distancia2=1, D_critica=15, D_alerta=40;
 
 /* Configuracion*/
@@ -131,7 +128,7 @@ void main()
 /*---------------*/
 
    while(TRUE){
-      for(i=0;i<=1;i++){//no se porque pero sin calcular primero las dos distancas luego no se comparan bien
+      for(i=0;i<=1;i++){//Leemos la distancia del objeto que esta delante
          disparo();
          
          if(Nuevopulso){
@@ -145,8 +142,6 @@ void main()
             else{
                distancia2=distancia;
             }
-          //  write_eeprom(dir,distancia); // BORRAR!
-            dir++;
    
             Nuevopulso=0;
             if(sfr==0){
@@ -159,77 +154,81 @@ void main()
          }
       }
       
-     if(primero&& (dato==0x20EF)){
-        parar();
+     if(primero){ //Si ha leido un dato
+        //Comprobamos cual es
+         switch(dato){
+            case 1 :  orden = 1; break;//A ->
+            case 2 :  orden = 2; break;//M ->
+            case IR_DRCH_C :  orden = 3; break;//C ->
+            case IR_IZDA_A :  orden = 4; break;//A <-
+            case IR_IZDA_M :  orden = 5; break;//M <-
+            case IR_IZDA_C :  orden = 6; break;//C <-
+            case IR_INIC :  orden = 7; break;//I 
+            case IR_PARAR :  orden = 8; break;//P 
+            default    :   orden =0;  break;
+         }
+         
+         //Actuamos
+         
+         if((distancia1 > D_alerta)&&(distancia2 > D_alerta)){
+         switch(orden){
+            case 1   :  giroAbierto(1); break;
+            case 2   :  giroCerrado(1); break;
+            case 3   :  giroMov(1);     break;
+            case 4   :  giroAbierto(0); break;
+            case 5   :  giroCerrado(0); break;
+            case 6   :  giroMov(0);     break;
+            case 7   :  inic();         break;
+            case 8   :  parar();        break;
+            default  :  inic();         break;}
      }
      else{
-     
-     inic();
-     y=(float)(distancia1/distancia2);//relacion
-      if((distancia1>D_alerta)&&(distancia2>D_alerta)){
-         inic();
-      }
-      else{
-         if(distancia1<distancia2) { //Obstáculo a la derecha (Giro a la izquierda)
- 
-            if (distancia1>D_critica){
+         if(distancia1 < distancia2) { //Obstáculo a la derecha (Giro a la izquierda)
             
-              giroMov(1);
-            
-            }
-            
-            else if (distancia1 <= D_critica){
-            
-              giroCerrado(0);
-            
-            }
-            
-   //!         else {
-   //!         
-   //!            giroAbierto(1);
-   //!         
-   //!         }
-         
+            switch(orden){
+               case 4   :  giroAbierto(0); break;
+               case 5   :  giroCerrado(0); break;
+               case 6   :  giroMov(0);     break;
+               case 8   :  parar();        break;
+               default  :  if (distancia1 > D_critica){ 
+                              giroMov(1);
+                           }
+                           else if (distancia1 <= D_critica){
+                              giroCerrado(0);
+                           }          
+                           break;}
          }
-         
-         else if (distancia1>distancia2) { //Obstáculo a la izquierda (Girar a la derecha)
-
-            if (distancia2>D_critica){
-            
-              giroMov(0);
-            
-            }
-            
-            else if (distancia2 <= D_critica){
-            
-               giroCerrado(1);
-            
-            }
-            
-   //!         else {
-   //!         
-   //!            giroAbierto(1);
-   //!         
-   //!         }  
-         
+         else if (distancia1 > distancia2) { //Obstáculo a la izquierda (Girar a la derecha)
+             
+             switch(orden){
+               case 1   :  giroAbierto(1); break;
+               case 2   :  giroCerrado(1); break;
+               case 3   :  giroMov(1);     break;
+               case 8   :  parar();        break;
+               default  :  if (distancia2 > D_critica){
+                              giroMov(0); 
+                           }
+                           else if (distancia2 <= D_critica){ 
+                              giroCerrado(1);
+                              delay_ms(3000);
+                           }          
+                           break;}
          }       
-         
          else { //Obstáculo delante
-            
-            if (distancia1 <= D_critica){
-            
-              giroCerrado(0);
-            
-            }
-            
-   //!       else {
-   //!         
-   //!            giroAbierto(0);
-   //!         
-   //!         }       
+         
+            switch(orden){
+               case 2   :  giroCerrado(1); break;
+               case 5   :  giroCerrado(0); break;
+               case 8   :  parar();        break;
+               default  :  if (distancia1 <= D_critica){ 
+                              giroCerrado(0);
+                              delay_ms(3000);
+                           }         
+                           break;}
          }
-      }
-       }
+       }      
+     }
+       
        
    }
 
